9c38c6843c03edcf3193e946a6346f9327897cb8
anunam <anupamjana478@gmail.com>
2025-03-28 15:43:13 +0530

    [Anupam]Add: find the key which value is greatest

---
+++ src/main/java/mapinterface/MaxValueKeyFinder.java
@@ -0,0 +1,29 @@
+package mapinterface;
+
+import java.util.*;
+
+public class MaxValueKeyFinder {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 10);
+        map.put("B", 20);
+        map.put("C", 15);
+
+        String maxKey = findMaxKey(map);
+        System.out.println("Key with the highest value: " + maxKey);
+    }
+
+    public static String findMaxKey(Map<String, Integer> map) {
+        String maxKey = null;
+        int maxValue = Integer.MIN_VALUE;
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > maxValue) {
+                maxValue = entry.getValue();
+                maxKey = entry.getKey();
+            }
+        }
+        return maxKey;
+    }
+}
+
